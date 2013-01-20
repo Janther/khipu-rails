cc4507ab6ac404546c3eb0dee5a5aa05c03b7f1b
Klaus Hott Vidal <klahott@gmail.com>
2013-01-20 18:18:29 -0300

    initialized the test environment with dummy data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require "action_view/railtie"
 require "nokogiri"
 require 'khipu-rails'
+
+KhipuRails::Config.user_id = 1234
+KhipuRails::Config.api_key = '1234567890asdfghjkl'
