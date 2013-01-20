9230431a27ead272e4b4b8e3aa32c68a2ed1597d
Klaus Hott Vidal <klahott@gmail.com>
2013-01-20 18:17:25 -0300

    Added Configuration class to the gem

--- lib/khipu-rails.rb
+++ lib/khipu-rails.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 require "khipu_rails/version"
+require "khipu_rails/config"
 require "khipu_rails/button_helper"
 
 ActionView::Base.send :include, KhipuRails::ButtonHelper
+
+module KhipuRails
+end
---
+++ lib/khipu_rails/config.rb
@@ -0,0 +1,33 @@
+module KhipuRails
+  class Config
+    def self.khipu_images
+      {
+        "50x25"    => "https://s3.amazonaws.com/static.khipu.com/buttons/50x25.png",
+        "100x25"   => "https://s3.amazonaws.com/static.khipu.com/buttons/100x25.png",
+        "100x50"   => "https://s3.amazonaws.com/static.khipu.com/buttons/100x50.png",
+        "150x25"   => "https://s3.amazonaws.com/static.khipu.com/buttons/150x25.png",
+        "150x50"   => "https://s3.amazonaws.com/static.khipu.com/buttons/150x50.png",
+        "150x75"   => "https://s3.amazonaws.com/static.khipu.com/buttons/150x75.png",
+        "150x75-B" => "https://s3.amazonaws.com/static.khipu.com/buttons/150x75.png",
+        "200x50"   => "https://s3.amazonaws.com/static.khipu.com/buttons/200x50.png",
+        "200x75"   => "https://s3.amazonaws.com/static.khipu.com/buttons/200x75.png"
+      }
+    end
+
+    def self.user_id= value
+      @@user_id = value
+    end
+
+    def self.api_key= value
+      @@api_key = value
+    end
+
+    def self.user_id
+      @@user_id
+    end
+
+    def self.api_key
+      @@api_key
+    end
+  end
+end
