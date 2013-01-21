16afb7cacb90b0505d13210f861c85a23ecc139e
Klaus Hott Vidal <klahott@gmail.com>
2013-01-21 11:15:50 -0300

    Added the KhipuRails module in its own file.

--- lib/khipu-rails.rb
+++ lib/khipu-rails.rb
@@ -1,9 +1,7 @@
 # encoding: utf-8
+require "khipu_rails"
 require "khipu_rails/version"
 require "khipu_rails/config"
 require "khipu_rails/button_helper"
 
 ActionView::Base.send :include, KhipuRails::ButtonHelper
-
-module KhipuRails
-end
---
+++ lib/khipu_rails.rb
@@ -0,0 +1,2 @@
+module KhipuRails
+end
