911b4fb99ed191597ef7d53a1481c1b11276773c
Klaus Hott Vidal <klahott@gmail.com>
2013-01-21 11:17:58 -0300

    Fixed the test that checks the helper addition.

--- spec/khipu_rails_spec.rb
+++ spec/khipu_rails_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 
 describe KhipuRails do
   it "adds the khipu_button helper to the view" do
-    ActionView::Base.method_defined? :khipu_button
+    ActionView::Base.method_defined?(:khipu_button).should == true
   end
 end
