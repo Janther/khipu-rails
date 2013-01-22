7428f56a4cac90e2f3494d30344567165198e7fc
Klaus Hott Vidal <klahott@gmail.com>
2013-01-21 23:02:48 -0300

    Fixed a redirection for the 150x75-B button

--- lib/khipu_rails/config.rb
+++ lib/khipu_rails/config.rb
@@ -8,7 +8,7 @@ module KhipuRails
         "150x25"   => "https://s3.amazonaws.com/static.khipu.com/buttons/150x25.png",
         "150x50"   => "https://s3.amazonaws.com/static.khipu.com/buttons/150x50.png",
         "150x75"   => "https://s3.amazonaws.com/static.khipu.com/buttons/150x75.png",
-        "150x75-B" => "https://s3.amazonaws.com/static.khipu.com/buttons/150x75.png",
+        "150x75-B" => "https://s3.amazonaws.com/static.khipu.com/buttons/150x75-B.png",
         "200x50"   => "https://s3.amazonaws.com/static.khipu.com/buttons/200x50.png",
         "200x75"   => "https://s3.amazonaws.com/static.khipu.com/buttons/200x75.png"
       }
