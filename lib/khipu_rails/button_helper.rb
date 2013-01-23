8cac5f931fd6697df3fbbf5c4b4de58a57fdb3be
Klaus Hott Vidal <klahott@gmail.com>
2013-01-23 20:00:04 -0300

    Added some extra nokogiri usage preparing khipu_button v2 (not TDD)

--- lib/khipu_rails/button_helper.rb
+++ lib/khipu_rails/button_helper.rb
@@ -15,7 +15,7 @@ module KhipuRails
       button_image = KhipuRails.config.button_images()[options[:button_image]] || options[:button_image]
 
       form_tag 'https://khipu.com/payment/api/createPaymentPage', authenticity_token: false do
-        [].tap do |i|
+        fields = [].tap do |i|
           i << hidden_field_tag(:receiver_id, options[:receiver_id])
           i << hidden_field_tag(:subject, subject)
           i << hidden_field_tag(:body, options[:body])
@@ -26,13 +26,36 @@ module KhipuRails
           i << hidden_field_tag(:transaction_id, options[:transaction_id])
           i << hidden_field_tag(:payer_email, options[:payer_email])
           i << hidden_field_tag(:picture_url, options[:picture_url])
-          i << hidden_field_tag(:hash, khipu_hash(options))
+        end.join.html_safe
+
+        ng = Nokogiri::HTML.parse fields
+
+        hash_fields = {
+          receiver_id: get_value(ng, :receiver_id),
+          subject: get_value(ng, :subject),
+          body: get_value(ng, :body),
+          amount: get_value(ng, :amount),
+          return_url: get_value(ng, :return_url),
+          cancel_url: get_value(ng, :cancel_url),
+          custom: get_value(ng, :custom),
+          transaction_id: get_value(ng, :transaction_id),
+          picture_url: get_value(ng, :picture_url),
+          payer_email: get_value(ng, :payer_email),
+          secret: options[:secret] || KhipuRails.config.receivers[options[:receiver_id]]
+        }
+
+        [fields].tap do |i|
+          i << hidden_field_tag(:hash, khipu_hash(hash_fields))
           i << image_submit_tag(button_image, name: :submit)
         end.join.html_safe
       end
     end
 
     private
+    def get_value inputs, attribute
+      inputs.css("input[name=#{attribute}]").attribute('value').value
+    end
+
     def khipu_hash options = {}
       raw = [
         "receiver_id=#{options[:receiver_id]}",
