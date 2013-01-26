c6996712ba8b5685be3c5b04f61eef28994fd187
Klaus Hott Vidal <klahott@gmail.com>
2013-01-26 13:34:56 -0300

    For better debug added test for the hash before the SHA1

--- spec/khipu_rails_spec.rb
+++ spec/khipu_rails_spec.rb
@@ -6,6 +6,30 @@ describe KhipuRails do
     ActionView::Base.method_defined?(:khipu_button).should == true
   end
 
+  it "Returns a uses the correct parameters for the Hash with the default values" do
+    KhipuRails.config = nil
+    KhipuRails.configure do |config|
+      config.receivers.merge! "123" => '1234567890asdfghjkl'
+    end
+
+    receiver = KhipuRails.config.receivers.first
+    values = KhipuRails.config.button_defaults
+
+    raw = "receiver_id=#{receiver.first}&"
+    raw += "subject=#{values[:subject]}&"
+    raw += "body=#{values[:body]}&"
+    raw += "amount=#{values[:amount]}&"
+    raw += "return_url=#{values[:return_url]}&"
+    raw += "cancel_url=#{values[:cancel_url]}&"
+    raw += "custom=#{values[:custom]}&"
+    raw += "transaction_id=#{values[:transaction_id]}&"
+    raw += "picture_url=#{values[:picture_url]}&"
+    raw += "payer_email=#{values[:payer_email]}&"
+    raw += "secret=#{receiver.last}"
+
+    KhipuRails.raw_hash.should == raw
+  end
+
   it "Returns a Hash with the default values" do
     KhipuRails.config = nil
     KhipuRails.configure do |config|
