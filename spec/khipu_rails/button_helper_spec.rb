12641f9d762f22c09adc1a403754c18ae8328f9b
Klaus Hott Vidal <klahott@gmail.com>
2013-01-28 12:45:02 -0300

    refactor of tests

--- spec/khipu_rails/button_helper_spec.rb
+++ spec/khipu_rails/button_helper_spec.rb
@@ -30,79 +30,65 @@ describe KhipuRails::ButtonHelper do
     end
 
     it "has an input called reciever_id" do
-      input = @button.css('form input[name=receiver_id]')
-      input.attribute('value').value.should == @receiver_id
+      attribute_value(:receiver_id).should == @receiver_id
     end
 
     it "has an input called subject" do
-      input = @button.css('form input[name=subject]')
-      input.attribute('value').value.should == 'minimal'
+      attribute_value(:subject).should == 'minimal'
     end
 
     it "has an input called body" do
-      input = @button.css('form input[name=body]')
-      input.attribute('value').value.should == ''
+      attribute_value(:body).should == ''
     end
 
     it "has an input called amount" do
-      input = @button.css('form input[name=amount]')
-      input.attribute('value').value.should == '2000'
+      attribute_value(:amount).should == '2000'
     end
 
     it "has an input called return_url" do
-      input = @button.css('form input[name=return_url]')
-      input.attribute('value').value.should == ''
+      attribute_value(:return_url).should == ''
     end
 
     it "has an input called cancel_url" do
-      input = @button.css('form input[name=cancel_url]')
-      input.attribute('value').value.should == ''
+      attribute_value(:cancel_url).should == ''
     end
 
     it "has an input called transaction_id" do
-      input = @button.css('form input[name=transaction_id]')
-      input.attribute('value').value.should == ''
+      attribute_value(:transaction_id).should == ''
     end
 
     it "has an input called custom" do
-      input = @button.css('form input[name=custom]')
-      input.attribute('value').value.should == ''
+      attribute_value(:custom).should == ''
     end
 
     it "has an input called payer_email" do
-      input = @button.css('form input[name=payer_email]')
-      input.attribute('value').value.should == ''
+      attribute_value(:payer_email).should == ''
     end
 
     it "has an input called picture_url" do
-      input = @button.css('form input[name=picture_url]')
-      input.attribute('value').value.should == ''
+      attribute_value(:picture_url).should == ''
     end
 
     it "has an input called hash" do
-      input = @button.css('form input[name=hash]')
-      input.attribute('type').value.should == 'hidden'
-      def get_value attribute
-        @button.css("form input[name=#{attribute}]").attribute('value').value
-      end
-      raw = "receiver_id=#{get_value :receiver_id}&"
-      raw += "subject=#{get_value :subject}&"
-      raw += "body=#{get_value :body}&"
-      raw += "amount=#{get_value :amount}&"
-      raw += "return_url=#{get_value :return_url}&"
-      raw += "cancel_url=#{get_value :cancel_url}&"
-      raw += "custom=#{get_value :custom}&"
-      raw += "transaction_id=#{get_value :transaction_id}&"
-      raw += "picture_url=#{get_value :picture_url}&"
-      raw += "payer_email=#{get_value :payer_email}&"
+      attribute_value(:hash, :type).should == 'hidden'
+
+      raw = "receiver_id=#{attribute_value :receiver_id}&"
+      raw += "subject=#{attribute_value :subject}&"
+      raw += "body=#{attribute_value :body}&"
+      raw += "amount=#{attribute_value :amount}&"
+      raw += "return_url=#{attribute_value :return_url}&"
+      raw += "cancel_url=#{attribute_value :cancel_url}&"
+      raw += "custom=#{attribute_value :custom}&"
+      raw += "transaction_id=#{attribute_value :transaction_id}&"
+      raw += "picture_url=#{attribute_value :picture_url}&"
+      raw += "payer_email=#{attribute_value :payer_email}&"
       raw += "secret=#{@secret}"
-      input.attribute('value').value.should == Digest::SHA1.hexdigest(raw)
+      attribute_value(:hash).should == Digest::SHA1.hexdigest(raw)
     end
 
     it "has an image submit" do
-      input = @button.css('form input[name=submit]')
-      input.attribute('type').value.should == 'image'
-      input.attribute('src').value.should == 'https://s3.amazonaws.com/static.khipu.com/buttons/50x25.png'
+      attribute_value(:submit, :type).should == 'image'
+      attribute_value(:submit, :src).should == 'https://s3.amazonaws.com/static.khipu.com/buttons/50x25.png'
     end
   end
 
@@ -125,79 +111,86 @@ describe KhipuRails::ButtonHelper do
     end
 
     it "has an input called reciever_id" do
-      input = @button.css('form input[name=receiver_id]')
-      input.attribute('value').value.should == @receiver_id.to_s
+      attribute_value(:receiver_id).should == @receiver_id.to_s
     end
 
     it "has an input called subject" do
-      input = @button.css('form input[name=subject]')
-      input.attribute('value').value.should == 'full'
+      attribute_value(:subject).should == 'full'
     end
 
     it "has an input called body" do
-      input = @button.css('form input[name=body]')
-      input.attribute('value').value.should == 'This is a full body'
+      attribute_value(:body).should == 'This is a full body'
     end
 
     it "has an input called amount" do
-      input = @button.css('form input[name=amount]')
-      input.attribute('value').value.should == '1000000'
+      attribute_value(:amount).should == '1000000'
     end
 
     it "has an input called return_url" do
-      input = @button.css('form input[name=return_url]')
-      input.attribute('value').value.should == 'http://foo.bar'
+      attribute_value(:return_url).should == 'http://foo.bar'
     end
 
     it "has an input called cancel_url" do
-      input = @button.css('form input[name=cancel_url]')
-      input.attribute('value').value.should == 'http://bar.foo'
+      attribute_value(:cancel_url).should == 'http://bar.foo'
     end
 
     it "has an input called transaction_id" do
-      input = @button.css('form input[name=transaction_id]')
-      input.attribute('value').value.should == 'asdf1234'
+      attribute_value(:transaction_id).should == 'asdf1234'
     end
 
     it "has an input called custom" do
-      input = @button.css('form input[name=custom]')
-      input.attribute('value').value.should == "Just testing\nwhat this api\tsupports."
+      attribute_value(:custom).should == "Just testing\nwhat this api\tsupports."
     end
 
     it "has an input called payer_email" do
-      input = @button.css('form input[name=payer_email]')
-      input.attribute('value').value.should == 'testing@khipu.com'
+      attribute_value(:payer_email).should == 'testing@khipu.com'
     end
 
     it "has an input called picture_url" do
-      input = @button.css('form input[name=picture_url]')
-      input.attribute('value').value.should == 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/402px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
+      attribute_value(:picture_url).should == 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/402px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
     end
 
     it "has an input called hash" do
-      input = @button.css('form input[name=hash]')
-      input.attribute('type').value.should == 'hidden'
-      def get_value attribute
-        @button.css("form input[name=#{attribute}]").attribute('value').value
-      end
-      raw = "receiver_id=#{get_value :receiver_id}&"
-      raw += "subject=#{get_value :subject}&"
-      raw += "body=#{get_value :body}&"
-      raw += "amount=#{get_value :amount}&"
-      raw += "return_url=#{get_value :return_url}&"
-      raw += "cancel_url=#{get_value :cancel_url}&"
-      raw += "custom=#{get_value :custom}&"
-      raw += "transaction_id=#{get_value :transaction_id}&"
-      raw += "picture_url=#{get_value :picture_url}&"
-      raw += "payer_email=#{get_value :payer_email}&"
+      attribute_value(:hash, :type).should == 'hidden'
+
+      raw = "receiver_id=#{attribute_value :receiver_id}&"
+      raw += "subject=#{attribute_value :subject}&"
+      raw += "body=#{attribute_value :body}&"
+      raw += "amount=#{attribute_value :amount}&"
+      raw += "return_url=#{attribute_value :return_url}&"
+      raw += "cancel_url=#{attribute_value :cancel_url}&"
+      raw += "custom=#{attribute_value :custom}&"
+      raw += "transaction_id=#{attribute_value :transaction_id}&"
+      raw += "picture_url=#{attribute_value :picture_url}&"
+      raw += "payer_email=#{attribute_value :payer_email}&"
       raw += "secret=#{@secret}"
-      input.attribute('value').value.should == Digest::SHA1.hexdigest(raw)
+      attribute_value(:hash).should == Digest::SHA1.hexdigest(raw)
     end
 
     it "has an image submit" do
-      input = @button.css('form input[name=submit]')
-      input.attribute('type').value.should == 'image'
-      input.attribute('src').value.should == 'https://s3.amazonaws.com/static.khipu.com/buttons/150x75-B.png'
+      attribute_value(:submit, :type).should == 'image'
+      attribute_value(:submit, :src).should == 'https://s3.amazonaws.com/static.khipu.com/buttons/150x75-B.png'
     end
   end
+
+  context "form with only options" do
+    before :all do
+      KhipuRails.config = nil
+      KhipuRails.configure do |config|
+        config.receivers.merge! "123" => '1234567890asdfghjkl',
+                                "321" => 'lkjhgfdsa0987654321'
+        config.button_defaults.merge! subject: 'Compra de Puntos Cumplo',
+                                      amount: 3000
+      end
+    end
+
+    it "allows developers to provide their own button_image" do
+      button = Nokogiri::HTML.parse(@view.khipu_button)
+      input = button.css('form input[name=receiver_id]')
+    end
+  end
+end
+
+def attribute_value input, attribute = :value, form = @button
+  form.css("form input[name=#{input.to_s}]").attribute(attribute.to_s).value
 end
